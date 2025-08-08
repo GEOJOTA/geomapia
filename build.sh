453e6fd4baa0836937288ac7bc31cbf2f593d9e5
GEOJOTA <juanpablolescanovillegas@gmail.com>
2025-08-08 18:37:45 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-set -e
-
 echo "Usando Node.js preinstalado en Render..."
-node --version
-npm --version
-
 echo "Instalando dependencias de Node.js..."
 npm install
-
 echo "Construyendo frontend con Vite..."
 npm run build
-
-echo "Instalando dependencias de Python..."
-pip install --upgrade pip
-pip install -r requirements.txt
 
