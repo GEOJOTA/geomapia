73e233e2299820af405514578ed3dbbd4ff888e5
GEOJOTA <juanpablolescanovillegas@gmail.com>
2025-08-08 16:09:04 -0400

    Update server.py

--- server.py
+++ server.py
@@ -4,12 +4,14 @@ import os
 app = Flask(__name__, static_folder='dist', static_url_path='')
 
 @app.route('/')
-def serve():
+def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory(app.static_folder, path)
+    if os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
