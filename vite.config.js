f8598a862a6ab184c1b5982d0f64854f4b8944b8
GEOJOTA <juanpablolescanovillegas@gmail.com>
2025-08-08 18:16:51 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // raíz en carpeta actual (opcional, por defecto así)
+  base: './', // base para build (útil si vas a servir estáticos desde subcarpeta)
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
+    port: 5173,
   },
-  build: {
-    outDir: 'dist',
-  },
-});
+})
